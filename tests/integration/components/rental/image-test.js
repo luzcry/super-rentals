7857c4b0d4f0341f48a5189ce73a48817572f35a
luzcry <mcrodriguezt618@gmail.com>
2020-08-28 20:36:10 -0500

    listing component

---
+++ tests/integration/components/rental/image-test.js
@@ -0,0 +1,28 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | rental/image', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders the given image', async function (assert) {
+    await render(hbs`<Rental::Image />`);
+
+    assert.equal(this.element.textContent.trim(), '');
+
+    // Template block usage:
+    await render(hbs`
+    <Rental::Image
+    src="/assets/images/teaching-tomster.png"
+    alt="Teaching Tomster"
+  />
+    `);
+
+    assert.dom('.image').exists();
+    assert
+      .dom('.image img')
+      .hasAttribute('src', '/assets/images/teaching-tomster.png');
+    assert.dom('.image img').hasAttribute('alt', 'Teaching Tomster');
+  });
+});
